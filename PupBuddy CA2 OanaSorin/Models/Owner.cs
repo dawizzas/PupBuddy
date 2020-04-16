c69241756dc44c61fc41fc56e67d887db56055c3
OannaCozma <cozmaoanna@gmail.com>
2020-04-16 12:55:10 +0100

    Model classes added: Owner, Puppy, OutdoorMeeting

---
+++ PupBuddy CA2 OanaSorin/Models/Owner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PupBuddy_CA2_OanaSorin.Models
+{
+    public class Owner
+    {
+        [Required]
+        public int Id { get; set; }
+        public string OwnerName { get; set; }
+        //pk 
+        [Key]
+        public string OwnerPhoneNumber { get; set; }
+    }
+}
---
+++ PupBuddy CA2 OanaSorin/Models/Puppy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PupBuddy_CA2_OanaSorin.Models
+{
+    public class Puppy
+    {
+        public int Id{ get; set; }
+
+        //pk
+        [Key]
+        public string OwnerPhoneNumber { get; set; }
+        [Required(ErrorMessage ="Please provide the breed")]
+        public string Breed { get; set; }
+        public string PupName { get; set; }
+        public string PupSize{ get; set; }
+        public decimal PupAge{ get; set; }
+
+
+    }
+}
